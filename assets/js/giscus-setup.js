0f47538a28defd2c0d582ff128fa5c675e7a53d1
RazinAleksandr <109418051+RazinAleksandr@users.noreply.github.com>
2025-10-12 20:59:16 +0000

    Deploying to gh-pages from @ RazinAleksandr/RazinAleksandr.github.io@4c6a4d054d90ab8e068d85838fe95959ea769561 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "RazinAleksandr/arazin",
+    "data-repo": "RazinAleksandr/RazinAleksandr.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
